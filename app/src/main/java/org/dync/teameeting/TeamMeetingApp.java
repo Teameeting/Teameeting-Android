9c8a639bedbb7697b9058ce3b6128ec5952a132b
dync <zhengxiaokai@dync.cc>
2016-01-19 17:41:18 +0800

    Revert "invoke sender.TMUnin when destroy"
    This reverts commit 4461a26263e136bf9e4389a43ae85de62d82d6de.

--- app/src/main/java/org/dync/teameeting/TeamMeetingApp.java
+++ app/src/main/java/org/dync/teameeting/TeamMeetingApp.java
@@ -12,13 +12,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings.Secure;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.widget.Toast;
 
 import org.anyrtc.Anyrtc;
 import org.dync.teameeting.bean.SelfData;
 import org.dync.teameeting.chatmessage.ChatMessageClient;
-import org.dync.teameeting.receiver.NetWorkReceiver;
 import org.dync.teameeting.sdkmsgclientandroid.msgs.TMMsgSender;
+import org.dync.teameeting.receiver.NetWorkReceiver;
 import org.dync.teameeting.utils.ScreenUtils;
 
 import cn.jpush.android.api.JPushInterface;
@@ -123,8 +124,6 @@ public class TeamMeetingApp extends Application {
 
     public void Destroy() {
         unregisterReceiver(mNetReceiver);
-        if (mMsgSender!=null)
-            mMsgSender.TMUnin();
     }
 
     /**
