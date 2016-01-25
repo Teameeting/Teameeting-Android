cbc471325e5aa27aad68723de2d78c664a57b846
dync <zhengxiaokai@dync.cc>
2016-01-25 12:45:31 +0800

    modify interface TMNotifyMsg
    add parameter tags to TMNotifyMsg,
    when invoke this method, tags is one of JMClientType.MCSENDTAGS_XXX

--- app/src/main/java/org/dync/teameeting/ui/activity/MeetingActivity.java
+++ app/src/main/java/org/dync/teameeting/ui/activity/MeetingActivity.java
@@ -59,7 +59,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Collections;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
 
@@ -791,7 +790,7 @@ public class MeetingActivity extends MeetingBaseActivity implements M2MultierEve
     public void OnRtcPublishOK(String publishId, String rtmpUrl, String hlsUrl) {
         //mAnyM2Mutlier.Subscribe(publishId, true);
         // Toast.makeText(this, "PublishOK id: " + publishId, Toast.LENGTH_SHORT).show();
-        int code = mMsgSender.TMNotifyMsg(mMeetingId, publishId);
+        int code = mMsgSender.TMNotifyMsg(mMeetingId, JMClientType.MCSENDTAGS_SUBSCRIBE, publishId);
 
         if (mDebug) {
             if (code == 0)
